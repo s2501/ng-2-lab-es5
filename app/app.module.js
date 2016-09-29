e811d32b7445971616a094827aaf7f2249f85b30
stanislav kumanov <s2501@abv.bg>
2016-09-29 23:22:31 +0300

    added laboratory for playing and tinkering with Angular 2 and ES5

---
+++ app/app.module.js
@@ -0,0 +1,33 @@
+(function(app) {
+    app.AppModule =
+        ng.core.NgModule({
+            imports: [
+                ng.platformBrowser.BrowserModule,
+                ng.forms.FormsModule,
+                ng.http.HttpModule,
+                app.routing
+            ],
+            declarations: [
+                app.AppComponent,
+                app.HeroListComponent,
+                app.HeroDetailsComponent,
+                app.DashboardComponent,
+                app.addHeroComponent
+            ],
+            providers: [
+                app.HeroicServices
+            ],
+            bootstrap: [
+                app.AppComponent
+            ]
+        })
+        .Class({
+            constructor: function() {}
+        });
+})(window.app || (window.app = {}));
+
+/*
+ * regarding ngForms
+ * you can read more at
+ * https://angular.io/docs/js/latest/guide/forms.html
+ */
